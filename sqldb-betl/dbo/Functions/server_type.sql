529b638c9036445a64a64c71d947d7c9aaf22059
Bas van den Berg <b.vdberg@c2h.nl>
2021-03-29 11:03:00 +0200

    server type

--- sqldb-betl/dbo/Functions/server_type.sql
+++ sqldb-betl/dbo/Functions/server_type.sql
@@ -1,11 +1,15 @@
 ﻿
+
 CREATE FUNCTION [dbo].[server_type]()
 RETURNS int
 AS
 BEGIN
 	return case 
-		when SERVERPROPERTY('Edition') < 4 then 15 -- on premise
-		when SERVERPROPERTY('Edition') >= 5 then 10 -- azure
+		when SERVERPROPERTY('EngineEdition') < 4 then 15 -- on premise
+		when SERVERPROPERTY('EngineEdition') >= 5 then 10 -- azure
 	end 
 
 END
+GO
+
+
